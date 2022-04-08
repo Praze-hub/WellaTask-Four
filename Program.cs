71d6dac77d2f4be8c57396f1ef5dc5f3f75e566e
Praze-hub <godspraiseokechukwu07@gmail.com>
2022-04-08 10:40:13 +0100

    Task four

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking for equality");
+            int sumOne = 22 + 15;
+            int sumTwo = 83 + 34;
+            int sumThree = 3 + 77;
+            if(sumOne < 100 ){
+                Console.WriteLine("True,The sum is" + sumThree);
+            }
+            else{
+                Console.WriteLine("False");
+            }
+            if(sumTwo < 100){
+                Console.WriteLine("True, the sum is" + sumTwo);
+            }
+            else{
+                Console.WriteLine("False");
+            }
+            if(sumThree < 100){
+                Console.WriteLine("True, the Sum is" + sumThree);
+            }
+            else{
+                Console.WriteLine("False");
+            }
+
+
+        }
+    }
+}
